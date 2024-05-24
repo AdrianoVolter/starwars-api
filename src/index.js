2b59b4bbee08b5c92187c5ecd4c9d299dd7f7549
AdrianoVolter <adriano.j.volter@hotmail.com>
2024-05-23 22:50:11 -0300

    conectando ao mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+
+mongoose.connect('mongodb://localhost/test')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
+    
+const Film = mongoose.model('Film', { 
+    title: String ,
+    description: String ,
+    image_url: String ,
+    trailer_url: String ,
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
